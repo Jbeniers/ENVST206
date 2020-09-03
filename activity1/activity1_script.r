9b900e75872a34667b74930cb0aaa9177ad3a4b9
Jbeniers <jbeniers@hamilton.edu>
2020-09-03 16:31:37 -0400

    added new example to script

--- activity1/activity1_script.r
+++ activity1/activity1_script.r
@@ -1,6 +1,33 @@
-#Activity 1
+# Activity 1
+
+# Question 2
 print("hello R world")
 
+# Question 3
+# Calculation 1
+# 16 plus 16
+16+16
 
+# 16 times 16
+16*16
 
+# 16 raised to the power of 16
+16^16
 
+# Calculation 2 (variable name)
+# name my number
+b <- 5000
+# multiply my number by 5
+b*5
+# divide my number by 5
+b/5
+
+# Calculation 3 (vector operations)
+# vector with 4 numbers 
+j <- c(16,25,92,100)
+# divide all numbers by 4 
+j/4
+
+# Extra Calculation
+# divide my number (variable name) by vector
+b/j
